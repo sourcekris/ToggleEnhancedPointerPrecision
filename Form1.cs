a6ad48386b0e44f2aa877dcc618e99a4ec9486ee
sourcekris <kris@sneakerz.org>
2024-07-06 19:12:53 +1000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace ToggleEnhancedPointerPrecision
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
