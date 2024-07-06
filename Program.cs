e0ac51b005a8dd00f62fc7c3532fe371fd5f3bb3
sourcekris <kris@sneakerz.org>
2024-07-06 22:31:01 +1000

    Basic functionality finished.

--- Program.cs
+++ Program.cs
@@ -5,11 +5,32 @@ using System.Threading;
 using System.Windows.Forms;
 using GlobalHotKeys.Native.Types;
 using GlobalHotKeys;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Reactive;
+using System.Security.Policy;
+using Microsoft.VisualBasic.Devices;
 
 namespace ToggleEnhancedPointerPrecision
 {
     internal static class Program
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool SystemParametersInfo(uint uiAction, uint uiParam, int[] pvParam, uint fWinIni);
+
+        // Consts from: https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfow
+
+        // Get and setting Mouse settings including acceleration (EPP).
+        private const uint SPI_GETMOUSE = 0x0003;
+        private const uint SPI_SETMOUSE = 0x0004;
+
+        // Get and setting Mouse acceleration value.
+        private const uint SPI_GETMOUSESPEED = 0x0070;
+        private const uint SPI_SETMOUSESPEED = 0x0071;
+
+        private const uint SPIF_UPDATEINIFILE = 0x01;
+        private const uint SPIF_SENDWININICHANGE = 0x02;
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -24,13 +45,42 @@ namespace ToggleEnhancedPointerPrecision
             using var shift1 = hotKeyManager.Register(VirtualKeyCode.VK_F12, Modifiers.Control);
 
             var form = new Form1();
+            ShowAccelState(form);
+
             using var subscription = hotKeyManager.HotKeyPressed
               .ObserveOn(SynchronizationContext.Current)
-              .Subscribe(hotKey =>
-                form.AppendText($"HotKey: Id = {hotKey.Id}, Key = {hotKey.Key}, Modifiers = {hotKey.Modifiers}{Environment.NewLine}")
+              .Subscribe(hotKey => ToggleEPP(form, hotKey)
               );
 
             Application.Run(form);
+        }
+
+        static void ToggleEPP(Form1 f, HotKey h)
+        {
+            int[] mouse = new int[3];
+            SystemParametersInfo(SPI_GETMOUSE, 0, mouse, 0);
+
+            if (mouse[2] == 1)
+            {
+                mouse[2] = 0;
+                f.AppendText($"Disabling EPP.{Environment.NewLine}");
+                SystemParametersInfo(SPI_SETMOUSE, 0, mouse, 0);
+            }
+            else
+            {
+                mouse[2] = 1;
+                f.AppendText($"Enabling EPP.{Environment.NewLine}");
+                SystemParametersInfo(SPI_SETMOUSE, 0, mouse, 0);
+            }
+            SystemParametersInfo(SPI_GETMOUSE, 0, mouse, 0);
+            f.AppendText($"Status of EPP now: {mouse[2] == 1}{Environment.NewLine}");
+        }
+
+        static void ShowAccelState(Form1 f)
+        {
+            int[] mouse = new int[3];
+            var res = SystemParametersInfo(SPI_GETMOUSE, 0, mouse, 0);
+            f.AppendText($"Status of EPP at startup: {mouse[2] == 1}{Environment.NewLine}");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,21 @@
+# Enhanced Pointer Precision Toggler
+
+A Windows application that lives in your system tray which can be used to toggle
+Windows Enhanced Pointer Precision (Mouse Acceleration) on and off via a global
+hotkey.
+
+Default hotkey is: Ctrl-F12
+
 ## Build
+
+Builds on Visual Studio Community 2022 w/.Net 8.
 
 This program uses GlobalHotKeys library from https://github.com/8/GlobalHotKeys
 ```
 dotnet nuget add source --name nuget.org https://api.nuget.org/v3/index.json
 dotnet add package GlobalHotKeys.Windows
 ```
+
+## Author:
+
+Kris Hunt ([@sourcekris](https://github.com/sourcekris))
